4dd8466542d290f79b0d2638e545368715a98477
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-10 22:48:45 +0530

    working on this directory on the concept of DOM MANIPULATION IN .js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello sharu");
+/*console.log("hello sharu");
 alert("hey  idiot!");
 console.log(window);
 window.alert("hey who are you?");
@@ -6,3 +6,27 @@ let heading = document.getElementById("good");
 console.log(heading);
 let head2 = document.getElementsByClassName("bad");
 console.log(head2);
+let elements = document.querySelector("button");
+console.dir(elements);
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText = h2.innerText + " ";
+let Box = document.querySelectorAll(".Box");
+console.dir(Box.innerText);
+Box.innerText = Box.innerText + "Good bro"
+let div = document.querySelector("div");
+console.log(div);
+let id = div.getAttribute("id");
+console.log(id); */
+let div = document.querySelector("div");
+console.log(div);
+console.log(div.setAttribute("id","class"));
+console.log(div.style);
+div.style.backgroundColor = "red";
+div.style.fontSize = "30px";
+div.style.width = "200px";
+div.style.visibility= "visible";
+div.style.boxShadow = "black";
+
+
+
