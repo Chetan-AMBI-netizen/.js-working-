f785443b3a5f6a9c1cd7caf3e0e0f11fc5fa2e47
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-17 18:43:52 +0530

    completed DOM manipulation in .ja

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-/*console.log("hello sharu");
-alert("hey  idiot!");
-console.log(window);
-window.alert("hey who are you?");
-let heading = document.getElementById("good");
-console.log(heading);
-let head2 = document.getElementsByClassName("bad");
-console.log(head2);
-let elements = document.querySelector("button");
-console.dir(elements);
-let h2 = document.querySelector("h2");
-console.dir(h2.innerText);
-h2.innerText = h2.innerText + " ";
-let Box = document.querySelectorAll(".Box");
-console.dir(Box.innerText);
-Box.innerText = Box.innerText + "Good bro"
-let div = document.querySelector("div");
-console.log(div);
-let id = div.getAttribute("id");
-console.log(id); 
-let div = document.querySelector("div");
-console.log(div);
-console.log(div.setAttribute("id","class"));
-console.log(div.style);
-div.style.backgroundColor = "red";
-div.style.fontSize = "30px";
-div.style.width = "200px";
-div.style.visibility= "visible";
-div.style.boxShadow = "black";
-*/
-let newbt = document.createElement("button");
-newbt.innerText = "Cleck here!";
-console.log(newbt);
-let di = document.querySelector("div");
-di.after(newbt);
-newbt.style.backgroundColor = "red";
+let newBTn = document.createElement("button");
+newBTn.innerText = "Click me!";
+newBTn.style.color = "white";
+newBTn.style.backgroundColor = "red";
+document.querySelector("body").prepend(newBTn);
+
+document.querySelector("p");
+
