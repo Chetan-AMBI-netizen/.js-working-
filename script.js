fbd38ba7805992586fe4b48d4fdfad8f1877debc
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-09-17 20:05:44 +0530

    done with events concept in .js

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 let btn = document.querySelector("#btn");
-btn.onclick = (e) => {
-    console.log(e.type);
-     console.log(e.target);
-      console.log(e.clientX);
-       console.log(e.clientY);
-    
-    
-    let para = document.querySelector("p");
-    para.onmouseover = (evt) =>{
-         console.log(evt.type);
-          console.log(evt.target);
-           console.log(evt.clientX);
-            console.log(evt.clientY);
+let mode = "light";
+
+btn.addEventListener("click",() =>{
+    if(mode === "light"){
+        mode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    } 
+
+    else{
+        mode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+
     }
+    console.log(mode);
 
-
-    console.log("error try agin!");
-    let a=403;
-    a++;
-    console.log(a);
-
-}
+    });
